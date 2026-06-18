7e33cab305a02f5bc31d106c63b5580a08aea85f
cy4strii <arinasushko@yandex.ru>
2026-06-19 00:29:57 +0300

    fix: remove dead auth/Redis code, clean up config

--- go.mod
+++ go.mod
@@ -6,15 +6,12 @@ require (
 	github.com/go-chi/chi/v5 v5.3.0
 	github.com/go-chi/httprate v0.15.0
 	github.com/joho/godotenv v1.5.1
-	github.com/redis/go-redis/v9 v9.20.1
 	go.uber.org/zap v1.28.0
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/zeebo/xxh3 v1.1.0 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
 )
--- internal/config/g1.go
+++ internal/config/g1.go
@@ -11,10 +11,7 @@ import (
 type Config struct {
 	Port              string
 	BackendURL        string
-	RedisAddr         string
-	RedisPassword     string
 	StreamingURL      string
-	RedisDB           int
 	RateLimitRequests int
 	RateLimitWindow   time.Duration
 	LogLevel          string
@@ -32,7 +29,6 @@ func Load() (*Config, error) {
 	// Загружаем .env файл (игнорируем ошибку, если файла нет)
 	_ = godotenv.Load()
 
-	redisDB, _ := strconv.Atoi(getEnv("REDIS_DB", "0"))
 	rateLimitRequests, _ := strconv.Atoi(getEnv("RATE_LIMIT_REQUESTS", "100"))
 	rateLimitWindow, _ := strconv.ParseInt(getEnv("RATE_LIMIT_WINDOW", "60"), 10, 64)
 
@@ -40,9 +36,6 @@ func Load() (*Config, error) {
 		Port:              getEnv("PORT", "8081"),
 		BackendURL:        getEnv("BACKEND_URL", "http://localhost:8080"),
 		StreamingURL:      getEnv("STREAMING_URL", "http://localhost:8082"),
-		RedisAddr:         getEnv("REDIS_ADDR", "localhost:6379"),
-		RedisPassword:     getEnv("REDIS_PASSWORD", ""),
-		RedisDB:           redisDB,
 		RateLimitRequests: rateLimitRequests,
 		RateLimitWindow:   time.Duration(rateLimitWindow) * time.Second,
 		LogLevel:          getEnv("LOG_LEVEL", "info"),
